ffab786f59d2c979dcd209000518c9ae381d77f9
wangfei <f.wang@muheda.com>
2019-09-02 18:28:53 +0800

    系统工具类提交

---
+++ system-module/systemkit/src/main/java/com/example/muheda/mhdsystemkit/sytemUtil/functionutil/CollectionUtil.java
@@ -0,0 +1,16 @@
+package com.example.muheda.mhdsystemkit.sytemUtil.functionutil;
+
+import java.util.List;
+
+/**
+ * @author zhangming
+ * @Date 2019/5/20 9:53
+ * @Description: 集合的工具类
+ */
+public class CollectionUtil {
+
+    public static boolean isEmpty(List list) {
+        return list == null || list.size() == 0;
+    }
+
+}
